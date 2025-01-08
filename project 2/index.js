fceff2d36b87b25de1505ae9ca46e1149b592a3a
rahulrameshm0 <iamrahulm23@gmail.com>
2025-01-09 00:34:16 +0530

    project to has completed

---
+++ project 2/index.js
@@ -0,0 +1,19 @@
+const input = document.getElementById("input")
+
+function reversedstring(str){
+    return str.split("").reverse().join("")
+}
+
+function checkpalindrome(){
+    const value = input.value
+    const reverse = reversedstring(value)
+
+    if (value === reverse){
+        alert("P A L I N D R O M E")
+    }
+    else{
+        alert('This sting is not palindrome!')
+    }
+
+    input.value = ""
+}
