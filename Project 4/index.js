c4891766b6ca13318e9bd6cee0aff8da3e6fa08a
rahulrameshm0 <iamrahulm23@gmail.com>
2025-01-10 21:07:06 +0530

    project 4 completed

--- Project 4/index.js
+++ Project 4/index.js
@@ -2,8 +2,15 @@ let second = 0
 let interval = null
 const timer = document.getElementById("time")
 
+
+function padStart(value){
+    return String(value).padStart(2, "0")
+}
+
 function setTimes(){
-    timer.innerHTML = second;
+    const minute = Math.floor(second/60)
+    const seconds = second % 60
+    timer.innerHTML = `${padStart(minute)}:${padStart(seconds)}`;
 }
 
 function startTime(){
@@ -12,13 +19,16 @@ function startTime(){
 }
 
 function startClock(){
+    if (interval) stopClock()
     interval = setInterval(startTime,1000)
 }
 
 function stopClock(){
-
+   clearInterval(interval)
 }
 
 function resetClock(){
-
+    stopClock()
+    second = 0;
+    setTimes()
 }
